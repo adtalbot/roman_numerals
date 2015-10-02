ad64d997f96ef148d834637595bdc5d2bbc57f89
audrie <audrie.talbot@gmail.com>
2015-10-02 09:47:01 -0700

    Refactored main function

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,55 @@
+// var romanNumerals = function(userInput) {
+//   var i = "";
+//   if(!multOf5) {
+//
+//     for(var j = 1; j < 10; j++) {
+//       if(userInput === j) {
+//         i += "I";
+//         return i;
+//       } else {
+//         i += "I";
+//       }
+//     }
+//   } else if(multOf5) {
+//
+//       for(var j = 5; j < 100; j+= 5) {
+//         if(userInput === j) {
+//           i += "V";
+//           return i;
+//         } else {
+//           i += "V";
+//         }
+//       }
+//   }
+// };
+//
+// var multOf5 = function(userInput) {
+//   if(userInput % 5 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
 var romanNumerals = function(userInput) {
-  var i = "";
-  if(userInput % 5 != 0) {
+  var index;
+  var units = 0;
+  var unitsArr = [];
+  var remainder = 0;
+  var symNum = [100,50,10,5,1];
 
-    for(var j = 1; j < 10; j++) {
-      if(userInput === j) {
-        i += "I";
-        return i;
-      } else {
-        i += "I";
-      }
-    }
-  } else if(userInput % 5 === 0) {
-
-      for(var j = 5; j < 100; j+= 5) {
-        if(userInput === j) {
-          i += "V";
-          return i;
-        } else {
-          i += "V";
-      }
-    }
+  for(index = 0; index < symNum.length; index++) {
+    units = parseInt(userInput / symNum[index]);
+    remainder = userInput % symNum[index];
+    unitsArr.push(units);
+    userInput = remainder;
   }
+  return unitsArr;
 };
+
+
+
+
 
 $(document).ready(function() {
   $("form#romanNumerals").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,22 @@
+// describe("romanNumerals", function() {
+//   it("converts the number one into a roman numeral", function() {
+//     expect(romanNumerals(1)).to.equal("I");
+//   });
+//
+//   it("adds additional I's for numbers under 5", function() {
+//     expect(romanNumerals(2)).to.equal("II");
+//   });
+//
+//   it("for 5 it returns V", function() {
+//     expect(romanNumerals(5)).to.equal("V");
+//   });
+// });
+
 describe("romanNumerals", function() {
-  it("converts the number one into a roman numeral", function() {
-    expect(romanNumerals(1)).to.equal("I");
+  it("returns an array with one unit", function() {
+    expect(romanNumerals(100)).to.eql([1,0,0,0,0]);
   });
-
-  it("adds additional I's for numbers under 5", function() {
-    expect(romanNumerals(2)).to.equal("II");
-  });
-
-  it("for 5 it returns V", function() {
-    expect(romanNumerals(5)).to.equal("V");
+  it("returns an array with two units", function() {
+    expect(romanNumerals(150)).to.eql([1,1,0,0,0]);
   });
 });

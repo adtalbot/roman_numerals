6008588c2bf30a779fb1400850982455d05865df
audrie <audrie.talbot@gmail.com>
2015-10-02 09:49:56 -0700

    Passes specs for all test cases

--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,17 @@
-// describe("romanNumerals", function() {
-//   it("converts the number one into a roman numeral", function() {
-//     expect(romanNumerals(1)).to.equal("I");
-//   });
-//
-//   it("adds additional I's for numbers under 5", function() {
-//     expect(romanNumerals(2)).to.equal("II");
-//   });
-//
-//   it("for 5 it returns V", function() {
-//     expect(romanNumerals(5)).to.equal("V");
-//   });
-// });
-
 describe("romanNumerals", function() {
   it("returns an array with one unit", function() {
     expect(romanNumerals(100)).to.eql([1,0,0,0,0]);
   });
   it("returns an array with two units", function() {
     expect(romanNumerals(150)).to.eql([1,1,0,0,0]);
+  });
+  it("returns an array with three units", function() {
+    expect(romanNumerals(160)).to.eql([1,1,1,0,0]);
+  });
+  it("returns an array with four units", function() {
+    expect(romanNumerals(165)).to.eql([1,1,1,1,0]);
+  });
+  it("returns an array with five units", function() {
+    expect(romanNumerals(166)).to.eql([1,1,1,1,1]);
   });
 });
